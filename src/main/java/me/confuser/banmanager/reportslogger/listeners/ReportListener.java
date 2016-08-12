f27134c455eee2342eda31d0fa8f7845654c300c
James Mortemore <james.mortemore@burberry.com>
2016-08-12 12:52:04 +0100

    Delete log reference when report deleted

--- src/main/java/me/confuser/banmanager/reportslogger/listeners/ReportListener.java
+++ src/main/java/me/confuser/banmanager/reportslogger/listeners/ReportListener.java
@@ -2,7 +2,9 @@ package me.confuser.banmanager.reportslogger.listeners;
 
 import me.confuser.banmanager.bukkitutil.listeners.Listeners;
 import me.confuser.banmanager.data.PlayerReportData;
+import me.confuser.banmanager.events.PlayerReportDeletedEvent;
 import me.confuser.banmanager.events.PlayerReportedEvent;
+import me.confuser.banmanager.internal.ormlite.stmt.DeleteBuilder;
 import me.confuser.banmanager.reportslogger.ReportsLogger;
 import me.confuser.banmanager.reportslogger.data.LogData;
 import me.confuser.banmanager.reportslogger.data.ReportLogData;
@@ -30,6 +32,21 @@ public class ReportListener extends Listeners<ReportsLogger> {
       } catch (SQLException e) {
         e.printStackTrace();
       }
+    }
+  }
+
+  @EventHandler
+  public void reportDeleted(PlayerReportDeletedEvent event) {
+    int id = event.getReport().getId();
+
+    DeleteBuilder<ReportLogData, Integer> builder = plugin.getReportLogStorage().deleteBuilder();
+
+    try {
+      builder.where().eq("report_id", id);
+      builder.delete();
+    } catch (SQLException e) {
+      e.printStackTrace();
+      plugin.getLogger().warning("Failed to delete report associations for " + id);
     }
 
   }
